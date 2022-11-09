b5ae51e361cf2d7b6134b6634d9b37aaaa3a1f1d
thiagolluiz <delimaluiz.thiago@gmail.com>
2022-11-09 20:38:22 -0300

    Deploy

--- newYearChaos/newYearChaos.js
+++ newYearChaos/newYearChaos.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -27,18 +27,38 @@ function readLine() {
  */
 
 function minimumBribes(q) {
-    // Write your code here
+  //
+  
+  let bribe = 0;
+  let chaotic = false;
 
+  for (let i = q.length - 1; i >= 0; i--) {
+    if (q[i] - i > 3) chaotic = true;
+    for (let j = q[i] - 2; j < i; j++) {
+      if (q[j] > q[i]) bribe++;
+    }
+  }
+
+  if (chaotic) {
+    console.log("Too chaotic");
+  } else {
+    console.log(bribe);
+  }
+
+  //
 }
 
 function main() {
-    const t = parseInt(readLine().trim(), 10);
+  const t = parseInt(readLine().trim(), 10);
 
-    for (let tItr = 0; tItr < t; tItr++) {
-        const n = parseInt(readLine().trim(), 10);
+  for (let tItr = 0; tItr < t; tItr++) {
+    const n = parseInt(readLine().trim(), 10);
 
-        const q = readLine().replace(/\s+$/g, '').split(' ').map(qTemp => parseInt(qTemp, 10));
+    const q = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((qTemp) => parseInt(qTemp, 10));
 
-        minimumBribes(q);
-    }
+    minimumBribes(q);
+  }
 }
